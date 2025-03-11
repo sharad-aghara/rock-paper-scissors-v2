91ef7a6a1066ff0be4a0c6d8d008850e89706e03
Sharad Aghara <sharadaghara24@gmail.com>
2025-03-12 00:45:29 +0530

    [+features]: display player and cpu choices, display scores and live score updates, alert on winning. (added methods and markup for the features)

--- main.js
+++ main.js
@@ -1,39 +1,87 @@
 const actions = ['rock', 'paper', 'scissors'];
-let scores = [0, 0, 0]; // 0: tie, 1: user, 2; cpu
+const imgItemUrl = ['../icons/stone.png', '../icons/paper.png', '../icons/scissors.png'];
+const imgItemAlt = ['rock', 'paper', 'scissors'];
+
+let scores = [0, 0, 0]; // 0: tie, 1: player, 2; cpu
+const scoreFormat = ['Player: ', 'CPU: ', 'Tie: '];
 
 function getCPUChoice(numberOfActions) {
     return Math.floor(Math.random() * numberOfActions);
 }
 
-function getUserChoice(buttonValue) {
-    let userChoice = parseInt(buttonValue.value);
-    let cpuChoice = getCPUrChoice(actions.length);
-    chooseWinner(userChoice, cpuChoice);   
+function getPlayerChoice(buttonValue) {
+    let playerChoice = parseInt(buttonValue.value);
+    let cpuChoice = getCPUChoice(actions.length);
+
+    displayChoices(playerChoice, cpuChoice);
+    chooseWinner(playerChoice, cpuChoice);   
 }
 
-function chooseWinner(userChoice, cpuChoice) {
+function chooseWinner(playerChoice, cpuChoice) {
     // tie
-    if (userChoice == cpuChoice) {
-        scores[0] += 1;
+    if (playerChoice == cpuChoice) {
+        scores[2] += 1;
     } 
-    // user wins
-    else if((cpuChoice == 0 && userChoice == 1) || (cpuChoice == 1 && userChoice == 2) && (cpuChoice == 2 && userChoice == 0)) {
-        scores[1] += 1;
+    // player wins
+    else if((cpuChoice == 0 && playerChoice == 1) || (cpuChoice == 1 && playerChoice == 2) && (cpuChoice == 2 && playerChoice == 0)) {
+        scores[0] += 1;
     } 
     // cpu wins
     else {
-        scores[2] += 1;
+        scores[1] += 1;
     }
 
     checkWinner();
+    updateScore();
 }
 
 function checkWinner() {
     scores.forEach(element => {
         if (element > 4) {
-            // add code to display winner
-
-
+            setTimeout(() => {
+                alert(scoreFormat[scores.indexOf(element)] + ' wins!');
+                scores = [0, 0, 0];
+                updateScore();
+            }, 1000);
         }
     });
+}
+
+function displayChoices(playerChoice, cpuChoice) {
+    console.log('displayChoices' + playerChoice + ' ' + cpuChoice);
+    
+    let imgCPU = document.querySelector('#imgCPU');
+    let imgPlayer = document.querySelector('#imgPlayer');
+
+    imgCPU.src = imgItemUrl[cpuChoice];
+    imgCPU.alt = imgItemAlt[cpuChoice];
+
+    imgPlayer.src = imgItemUrl[playerChoice];
+    imgPlayer.alt = imgItemAlt[playerChoice];
+
+    showGrid();
+}
+
+function updateScore() {
+    let playerScore = document.querySelector('#txtPlayerScore');
+    let cpuScore = document.querySelector('#txtCPUScore');
+    let tieScore = document.querySelector('#txtTieScore');
+
+    playerScore.textContent = scores[0];
+    cpuScore.textContent = scores[1];
+    tieScore.textContent = scores[2];
+}
+
+function showGrid() {
+    let choiceGrid = document.querySelector('#choiceGrid');
+    choiceGrid.style.display = '';
+    let imgGrid = document.querySelector('#imgGrid');
+    imgGrid.style.display = '';
+}
+
+function hideGrid() {
+    let choiceGrid = document.querySelector('#choiceGrid');
+    choiceGrid.style.display = 'none';
+    let imgGrid = document.querySelector('#imgGrid');
+    imgGrid.style.display = 'none';
 }
